0916a896a2fb477a29e3bf5cd5bca00b164089d1
Tushit <ch25btech11043@iith.ac.in>
2026-08-20 07:11:02 +0530

    Time: 0 ms (100%), Space: 14 MB (30.56%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        long long x=0;
+        for(int i=0;i<nums.size();i++){
+            x=x^nums[i];
+        }
+        long long y=(x&(x-1))^x;
+        int b1=0,b2=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]&y) b1=b1^nums[i];
+            else b2=b2^nums[i];
+        }
+        return {b1,b2};
+    }
+};
